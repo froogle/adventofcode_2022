2f4f1043ade2bb8e27921255e3926439d9ca1351
Pete Wright <peterjwright@gmail.com>
2022-12-07 09:57:20 +0000

    More refactoring for day 7

--- adventofcode/days/Day7.swift
+++ adventofcode/days/Day7.swift
@@ -8,36 +8,51 @@
 import Foundation
 class Day7 : AdventDay {
     
+    var sizes = [String:Int]()
+    var filePaths = [String]()
+    var totalSize = 0
+    
+
+    
     func execute() {
         let handler = InputHandler(
             path: "/Users/froogle/develops/adventofcode/adventofcode/inputs/day7_1"
         )
         
-        var sizes = [String:Int]()
-        var filePaths = [String]()
-        var totalSize = 0
         handler.parse{ line in
             
             let words = line.split(separator: " ")
             if words[1] == "cd" {
-                if words[2] == ".." {
-                    _ = filePaths.popLast()
-                } else {
-                    filePaths.append( String(words[2]) )
-                }
+                processCd(String(words[2]))
             } else if words[0] != "$" {
-                let fileSize = Int(words[0]) ?? 0
-
-                var qualifiedPath = "/"
-                for filePath in filePaths {
-                    qualifiedPath = "\(qualifiedPath)/\(filePath)"
-                    sizes[qualifiedPath] = (sizes[qualifiedPath] ?? 0) + fileSize
-                }
-                totalSize += fileSize
+                processFile(String(words[0]))
             }
         }
         sizes["/"] = totalSize
+    
+        calculateAnswers()
         
+    }
+    
+    func processCd(_ cdPath: String) {
+        if cdPath == ".." {
+            _ = filePaths.popLast()
+            return
+        }
+        filePaths.append( String(cdPath) )
+    }
+    
+    func processFile(_ fileSize: String) {
+        let fileSize = Int(fileSize) ?? 0
+        var qualifiedPath = "/"
+        for filePath in filePaths {
+            qualifiedPath = "\(qualifiedPath)/\(filePath)"
+            sizes[qualifiedPath] = (sizes[qualifiedPath] ?? 0) + fileSize
+        }
+        totalSize += fileSize
+    }
+    
+    func calculateAnswers() {
         let maxUsed = 70000000 - 30000000
         let totalUsed = sizes["/"]!
         let needToFree = totalUsed - maxUsed
@@ -56,6 +71,5 @@ class Day7 : AdventDay {
         
         print(part1Answer)
         print(part2Answer)
-        
     }
 }
